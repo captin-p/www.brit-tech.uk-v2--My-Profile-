93898e20ac73de19ec590ca59fa3c19fdf47341a
captin-p <101121458+captin-p@users.noreply.github.com>
2026-09-23 09:56:32 +0000

    Publish Windows camera troubleshooting guide

--- app/guides/page.tsx
+++ app/guides/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { guides } from "@/data/guides";
 import { moreGuides } from "@/data/guides-more";
 import { latestGuides } from "@/data/guides-latest";
+import { guides20260923 } from "@/data/guides-2026-09-23";
 import { pcTurnsOnNoDisplayGuide } from "@/data/guides/pc-turns-on-no-display";
 import { GuideBrowser } from "@/components/guides/GuideBrowser";
 
@@ -12,7 +13,7 @@ export const metadata: Metadata = {
 };
 
 export default function GuidesPage() {
-  const allGuides = [pcTurnsOnNoDisplayGuide, ...latestGuides, ...guides, ...moreGuides];
+  const allGuides = [...guides20260923, pcTurnsOnNoDisplayGuide, ...latestGuides, ...guides, ...moreGuides];
 
   return (
     <main className="mx-auto max-w-6xl px-6 py-16 md:py-24">
